7f29e93e0f50b9d8f7432d5f3d18bcdbaed3610b
Alexander <forcewizu@gmail.com>
2018-09-08 17:31:27 +0300

    add structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { HelloComponent } from './hello/hello.component';
-import { MenuComponent } from './menu/menu.component';
-import { ButtonMembershipComponent } from './button-membership/button-membership.component';
-import { ButtonMapComponent } from './button-map/button-map.component';
-import { ButtonBookRoomComponent } from './button-book-room/button-book-room.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { HelloComponent } from './pages/hello/hello.component';
+import { MenuComponent } from './common/menu/menu.component';
+import { ButtonMembershipComponent } from './common/buttons/button-membership/button-membership.component';
+import { ButtonMapComponent } from './common/buttons/button-map/button-map.component';
+import { ButtonBookRoomComponent } from './common/buttons/button-book-room/button-book-room.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    HelloComponent,
-    MenuComponent,
-    ButtonMembershipComponent,
-    ButtonMapComponent,
-    ButtonBookRoomComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavigationComponent,
+        HelloComponent,
+        MenuComponent,
+        ButtonMembershipComponent,
+        ButtonMapComponent,
+        ButtonBookRoomComponent
+    ],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
